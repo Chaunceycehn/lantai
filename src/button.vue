ba9ded5ce58e0b18588a516c5abf529dfb21dd81
chenchuang <chchuang@foxmail.com>
2019-12-06 15:09:05 +0800

    安装parcel parcel打包

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="g-button" id="g-button">中文显44ttt不能</button>
+</template>
+
+<script>
+    export default {}
+</script>
+<style lang="scss">
+    .g-button{
+        height: var(--button-height);
+        padding: 0 1em;
+        /*font: inherit;*/
+        font-size: var(--font-size);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:active{
+            background: var(--button-active-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+</style>
